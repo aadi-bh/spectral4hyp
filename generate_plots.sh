e06461e1471cf973e2089469448200247af52c12
Aadi <122440845+aadi-bh@users.noreply.github.com>
2023-11-23 07:33:28 +0530

    Change generate_plots.sh

--- generate_plots.sh
+++ generate_plots.sh
@@ -3,5 +3,12 @@
 ## TESTING
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+from past.builtins import execfile
 
+os.chdir("_site/plots/")
+execfile("burgers_filtered.py")
+
+with open("_site/index.html", 'w') as file:
+	file.write("Done generating plots.")
 print("Done generating plot scripts.")
