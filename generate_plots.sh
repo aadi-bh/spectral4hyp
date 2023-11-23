8c139218696612b6084c4c91e630bb47e4ce4d0e
Aadi <122440845+aadi-bh@users.noreply.github.com>
2023-11-23 07:44:28 +0530

    Fix generate_plots

--- generate_plots.sh
+++ generate_plots.sh
@@ -7,7 +7,7 @@ import os
 from past.builtins import execfile
 
 os.chdir("_site/plots/")
-execfile("burgers_filtered.py")
+execfile("../../burgers_filtered.py")
 
 with open("_site/index.html", 'w') as file:
 	file.write("Done generating plots.")
