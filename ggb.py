42d2fa13da9b5094949f36fe9518e0a2141e9ec4
Aadi B <122440845+aadi-bh@users.noreply.github.com>
2023-11-25 11:05:18 +0530

    Brute force fix integration

--- ggb.py
+++ ggb.py
@@ -12,6 +12,12 @@ def xi(y, a, b):
     return -1 + 2 * (y-a)/(b-a)
 
 '''
+Maps interval [-1, 1] to [a, b]
+'''
+def xi_inv(z, a, b):
+    return (b+a)/2 + z * (b-a)/2
+
+'''
 Weight function
 '''
 def w(x, lam):
@@ -32,24 +38,22 @@ def gam(n, lam):
     r /= gamma(lam) * gamma(2 * lam) * factorial(n) * (n+lam)
     return r
 
-def wtd_inner(uh, n, lam, a, b):
-    x = cgrid(2*len(uh)*(n+1), a, b)
-    z = xi(x, a, b)
-    dz = z[1] - z[0]
-    fx = ifft_at(x, uh)
+def ip(uh, n, lam, a, b):
+    z, wts = np.polynomial.legendre.leggauss(10*n+200);
+    x = xi_inv(z, a, b)
+    fx = ifft_at(x, uh).real
     Cz = C(z, n, lam)
     wz = w(z, lam)
-    # Trapezoidal rule, but w = 0 at +-1
-    return dz * np.sum(fx * Cz * wz)
+    return np.sum(wts * fx * Cz * wz)
 
 x = np.linspace(0, 2*pi, 16)
 u = np.ones(x.shape)
 uh = fft(u)
-print(wtd_inner(uh, 0, 1, 1,2) / gam(0,1))
-print(wtd_inner(uh, 1, 1, 1,2))
-print(wtd_inner(uh, 2, 1, 1,2))
-print(wtd_inner(uh, 3, 1, 1,2))
-print(wtd_inner(uh, 4, 1, 1,2))
+print(ip(uh, 0, 1, 1,2) / gam(0, 1))
+print(ip(uh, 1, 1, 1,2) / gam(1, 1))
+print(ip(uh, 2, 1, 1,2) / gam(2, 1))
+print(ip(uh, 3, 1, 1,2) / gam(3, 1))
+print(ip(uh, 4, 1, 1,2) / gam(4, 1))
 # TODO
 # Expand it in terms of the ggbs
 # Patch it back into the solution.
